c651514fd4013fd4b3f26e12d5a8894d04010f28
agrshch <98658900+agrshch@users.noreply.github.com>
2022-11-30 19:02:35 +0100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,103 +1,110 @@
-const SB_num = 20;
-const snowballs = [];
-const videosID = new Array(SB_num).fill('10924819024819384');
+const BACK_h = window.innerHeight * 1.4;
+const BACK_w = BACK_h * 1.78;
+const backTranslateRange = window.innerHeight*0.15;
 
-const SB_pics = new Array(4);
-const BACK_pics = new Array(3);
+const LAYERS = 3;
+const SNOWBALLS = new Array(LAYERS);
+const BACK_pics = new Array(LAYERS);
+const SB_num = 20;
+const SB_pics = new Array(16);
 let HAND_pic;
-let stoppped = false;
+
+let trXprev = 0;
+let trYprev = 0;
 
 function preload(){
-  for (let i = 0; i < SB_pics.length; i ++) SB_pics[i] = loadImage('images/s'+(i+1)+'.png');
-  for (let i = 0; i < BACK_pics.length; i ++) BACK_pics[i] = loadImage('images/back'+(i+1)+'.png');
-  HAND_pic = loadImage('images/hand.png');
+  for (let i = 0; i < SB_pics.length; i ++) SB_pics[i] = loadImage('images/sb'+(i+1)+'.svg');
+  for (let i = 0; i < BACK_pics.length; i ++) BACK_pics[i] = loadImage('images/back'+(i+1)+'.svg');
+  HAND_pic = loadImage('images/hand.svg');
 }
 
 function setup() {
-  noCursor();
+  // noCursor();
   imageMode(CENTER);
   rectMode(CENTER);
   createCanvas(windowWidth, windowHeight);
-  snowballs.push (new Array(Math.floor(SB_num*0.4)));
-  snowballs.push (new Array(Math.floor(SB_num*0.35)));
-  snowballs.push (new Array(SB_num - snowballs[0].length - snowballs[1].length));
-  // HAND_pic.resize(150,0);
+  SNOWBALLS[0] = new Array(Math.floor(SB_num*0.40));
+  SNOWBALLS[1] = new Array(Math.floor(SB_num*0.35));
+  SNOWBALLS[2] = new Array(SB_num - SNOWBALLS[0].length - SNOWBALLS[1].length);
+  HAND_pic.resize(150,0);
 
-  for (let i = 0; i < SB_pics.length; i ++) {
-    console.log(SB_pics[i].width);
-  }
-
-  for (let s = 0; s < snowballs.length; s++){
-    for (let i = 0; i < snowballs[s].length; i++){
+  for (let s = 0; s < SNOWBALLS.length; s++){
+    for (let i = 0; i < SNOWBALLS[s].length; i++){
       let inversion = (random() < 0.5);
       let x = random(width);
       let y = random(height);
-      let w = (width*0.1) / (snowballs.length - s);
+      let r = (width*0.2) / (SNOWBALLS.length - s+1);
       let speed = s+1;
       let pic = SB_pics[floor(random(SB_pics.length))];
-      snowballs[s][i] = new Snowball(x,y,w,speed,inversion,pic);
+      SNOWBALLS[s][i] = new Snowball(x,y,r,speed,inversion,pic);
     }
   }
 }
 
 function draw() {
-  if (stoppped) return;
-  for(let s = 0; s < snowballs.length; s++){
-    image(BACK_pics[s],width/2,height/2,width,height);
-    for(let i = 0; i < snowballs[s].length; i++){
-      snowballs[s][i].update();
-      snowballs[s][i].display();
+  let lerpVal = 0.05;
+  let trXdesired = map(mouseX,0,width, -backTranslateRange,backTranslateRange) * 0.3;
+  let trX = lerp(trXprev,trXdesired,lerpVal);
+
+  let trYdesired = map(mouseY,0,width, -backTranslateRange,backTranslateRange) * 0.2;
+  let trY = lerp(trYprev,trYdesired,lerpVal);
+
+  for(let s = 0; s < SNOWBALLS.length; s++){
+    push();
+    translate(trX * (s+0.5), trY * (s+0.3));
+    image(BACK_pics[s],width/2,height/2,BACK_w,BACK_h);
+    pop();
+    for(let i = 0; i < SNOWBALLS[s].length; i++){
+      SNOWBALLS[s][i].update();
+      SNOWBALLS[s][i].display();
     }
   }
 
-  for(let s = 0; s < snowballs.length; s++){
-    for(let i = 0; i < snowballs[s].length; i++){
-      if (snowballs[s][i].loc.y > height + snowballs[s][i].w && !snowballs[s][i].inverted) {
-        snowballs[s].splice(i,1);
+  trXprev = trX;
+  trYprev = trY;
+
+  for(let s = 0; s < SNOWBALLS.length; s++){
+    for(let i = 0; i < SNOWBALLS[s].length; i++){
+      if (SNOWBALLS[s][i].isOut('bottom')) {
+        SNOWBALLS[s].splice(i,1);
         let inversion = false;
         let x = random(width);
         let y = random(-100,-500);
-        let w = (width*0.1) / (snowballs.length - s);
+        let r = (width*0.2) / (SNOWBALLS.length - s+1);
         let speed = s+1;
         let pic = SB_pics[floor(random(SB_pics.length))];
-        snowballs[s].push(new Snowball(x,y,w,speed,inversion,pic));
+        SNOWBALLS[s].push(new Snowball(x,y,r,speed,inversion,pic));
       }
 
-      if (snowballs[s][i].loc.y < -snowballs[s][i].w && snowballs[s][i].inverted) {
-        snowballs[s].splice(i,1);
+      if (SNOWBALLS[s][i].isOut('top')) {
+        SNOWBALLS[s].splice(i,1);
         let inversion = true;
         let x = random(width);
         let y = height + random(100,500);
-        let w = (width*0.1) / (snowballs.length - s);
+        let r = (width*0.2) / (SNOWBALLS.length - s+1);
         let speed = s+1;
         let pic = SB_pics[floor(random(SB_pics.length))];
-        snowballs[s].push(new Snowball(x,y,w,speed,inversion,pic));
+        SNOWBALLS[s].push(new Snowball(x,y,r,speed,inversion,pic));
       }
     }
   }
-  push();
-  translate(mouseX, mouseY);
-  if (mouseY < height/2) rotate(PI);
-  image(HAND_pic,0,0);
-  pop();
 }
 
 function mousePressed(){
-  for (let s = 0; s < snowballs.length; s++){
-    for (let i = 0; i < snowballs[s].length; i++){
+  for (let s = SNOWBALLS.length-1; s >= 0; s--){
+    for (let i = SNOWBALLS[s].length-1; i >= 0; i--){
       let me = new p5.Vector(mouseX, mouseY);
-      let r = snowballs[s][i].w*0.5;
-      let ballCenter = new p5.Vector(snowballs[s][i].loc.x, snowballs[s][i].loc.y);
+      let r = SNOWBALLS[s][i].r*0.5;
+      let ballCenter = new p5.Vector(SNOWBALLS[s][i].loc.x, SNOWBALLS[s][i].loc.y);
       if(p5.Vector.dist(me,ballCenter) < r){
-        stoppped = true;
-        background(0,200);
-        rect(width/2,height/2,500,500,300);
+        showModalWithVideo();
+        return;
       }
     }
   }
 }
 
 
-// function showModalWithVideo(id) {
-//   console.log('Modal with video is shown! The video ID is '+id);
-// }
+function showModalWithVideo() {
+  console.log('Modal with video is shown!');
+}
--- snowball.js
+++ snowball.js
@@ -1,13 +1,14 @@
 class Snowball {
-  constructor(x,y,w,speed,inversion,pic){
+  constructor(x,y,r,speed,inversion,pic){
     this.maxSpeed = random(0.6,0.8)*speed;
+    this.maxAcc = 0.03;
     this.loc = new p5.Vector(x,y); // center
     this.vel = new p5.Vector(0,1);
     this.vel.setMag(this.maxSpeed);
-    this.w = random(w*0.9,w*1.1);
-    this.h = this.w;
+    this.acc = new p5.Vector(0,0);
+    this.r = r;
     this.pic = pic.get();
-    this.pic.resize(this.w, this.h);
+    this.pic.resize(this.r, this.r);
     this.offset = random(360);
 
     this.inverted = inversion;
@@ -16,18 +17,40 @@ class Snowball {
   }
 
   update(){
+    this.vel.add(this.acc);
+    this.vel.limit(this.maxSpeed);
     this.loc.add(this.vel);
+    this.acc.mult(0);
+
     this.rotate();
     this.wiggle();
+    this.seekCursor(this.r*2);
+  }
+
+  applyForce(force) {
+    this.acc.add(force);
   }
 
   wiggle(){
     let a = radians(this.offset+frameCount);
-    let w = new p5.Vector(sin(a), 0);
-    if (this.inverted) w.add(new p5.Vector(0,-1));
-    else w.add(new p5.Vector(0,1));
-    this.vel.add(w);
-    this.vel.limit(this.maxSpeed);
+    let desired = new p5.Vector(sin(a), 0);
+    if (this.inverted) desired.add(new p5.Vector(0,1));
+    else               desired.add(new p5.Vector(0,-1));
+    let steer = p5.Vector.sub(this.vel, desired);
+    steer.limit(this.maxAcc);
+    this.applyForce(steer);
+  }
+
+  seekCursor(range){
+    let dx = this.loc.x - mouseX;
+    let dy = this.loc.y - mouseY;
+    let d =  dx*dx+dy*dy;
+    if(d < range*range){
+      let desired = p5.Vector.sub(this.loc, new p5.Vector(mouseX, mouseY));
+      let steer = p5.Vector.sub(this.vel, desired);
+      steer.limit(this.maxAcc);
+      this.applyForce(steer);
+    }
   }
 
   rotate(){
@@ -44,5 +67,22 @@ class Snowball {
     rotate(this.angle);
     image(this.pic, 0,0);
     pop();
+  }
+
+  isOut(where){
+    if (where === 'top'       &&
+        this.loc.y < -this.r  &&
+        this.inverted === true){
+      console.log('im out on top');
+      return true;
+    }
+
+    if (where === 'bottom'             &&
+        this.loc.y > height+this.r  &&
+        this.inverted === false){
+      console.log('im out on bottom');
+      return true;
+    }
+
   }
 }
